9be59df64fa729fe1863b59a2131fcbc88364f41
AbhayK <agd.abhaykumar@gmail.com>
2026-05-12 11:24:04 +0530

    feat: update cron schedule to run every second

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-function createPrismaClient() {
-    const connectionString = process.env.DATABASE_URL!;
-    const adapter = new PrismaPg({ connectionString, max: 1 });
-    return new PrismaClient({ adapter });
-}
-
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
+
+function createPrismaClient() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    return new PrismaClient({ adapter });
+}
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
